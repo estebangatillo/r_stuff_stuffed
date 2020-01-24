0af17a4303e0c2359318abeb17333ed5855966fa
esteban <52689748+estebangatillo@users.noreply.github.com>
2020-01-23 22:14:58 -0800

    through chapter 2

--- Working with Data in the Tidyverse.R
+++ Working with Data in the Tidyverse.R
@@ -127,6 +127,219 @@ desserts %>%
 
 
 
+# Edit code to fix the parsing error 
+desserts <- read_csv("desserts.csv",
+                     col_types = cols(
+                       uk_airdate = col_date(format = "%d %B %Y"),
+                       technical = col_number()
+                     ),
+                     na = c("", "NA", "N/A")
+)
+
+# View parsing problems
+problems(desserts)
+
+
+
+
+
+
+
+
+
+
+
+# Cast result a factor
+desserts <- read_csv("desserts.csv", 
+                     na = c("", "NA", "N/A"),
+                     col_types = cols(
+                       uk_airdate = col_date(format = "%d %B %Y"),
+                       technical = col_number(),                       
+                       result = col_factor(levels = NULL)
+                     )
+)
+
+# Glimpse to view
+glimpse(desserts)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Count rows grouping by nut variable
+desserts %>% 
+  count(nut, sort = TRUE)
+
+# Edit code to recode "no nut" as missing
+desserts_2 <- desserts %>% 
+  mutate(nut = recode(nut, "filbert" = "hazelnut", 
+                      "no nut" =  NA_character_))
+
+# Count rows again 
+desserts_2 %>% 
+  count(nut, sort = TRUE)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Edit to recode tech_win as factor
+desserts <- desserts %>% 
+  mutate(tech_win = recode_factor(technical, `1` = 1,
+                                  .default = 0))
+
+# Count to compare values                      
+desserts %>% 
+  count(technical == 1, tech_win)
+
+
+
+
+
+
+
+
+###### Couldn't do this:::::
+
+
+#For series with 10 episodes, which showed the most growth in viewers from the premiere to the finale? Which showed the least?
+
+
+
+
+
+
+
+
+
+
+
+
+# Recode channel as factor: bbc (1) or not (0)
+ratings <- ratings %>% 
+  mutate(bbc = recode_factor(channel, 
+                             "Channel 4" = 0,
+                             .default = 1))
+
+# Select to look at variables to plot next
+ratings %>% 
+  select(series, channel, bbc, viewer_growth)
+
+# Make a filled bar chart
+ggplot(ratings, aes(x = series, y = viewer_growth, fill = bbc)) +
+  geom_col()
+
+
+
+
+
+
+
+
+
+
+
+# Move channel to front and drop 7-/28-day episode ratings
+ratings %>% 
+  select(channel,  everything(), -ends_with("day"))
+
+
+
+
+
+
+
+
+
+
+# Glimpse to see variable names
+glimpse(messy_ratings)
+
+# Load janitor
+library(janitor)
+
+# Reformat to snake case
+ratings <- messy_ratings %>%  
+  clean_names()
+
+# Glimpse cleaned names
+glimpse(ratings)
+
+
+
+
+
+
+
+
+
+
+
+# Adapt code to also rename 7-day viewer data
+viewers_7day <- ratings %>% 
+  select(series, viewers_7day_ = ends_with("7day"))
+
+# Glimpse
+glimpse(viewers_7day)
+
+
+
+
+
+
+
+
+
+
+
+# Adapt code to keep original order
+viewers_7day <- ratings %>% 
+  select(everything(), viewers_7day_ = ends_with("7day"), 
+         -ends_with("28day"))
+
+# Glimpse
+glimpse(viewers_7day)
+
+
+
+
+
+
+
+
+
+
+
+
+
+############################# CHAPTER 3 ################################################
+
+
+
+
+
 
 
 
